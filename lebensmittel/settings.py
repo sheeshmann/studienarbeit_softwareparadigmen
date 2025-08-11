56ee8297e4c9088d5483445b089fb1a410d1f600
sheeshmann <schliessmann.lukas@t-online.de>
2025-08-11 18:11:24 +0200

    Settings Datei geupdatet

--- lebensmittel/settings.py
+++ lebensmittel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a0d%@v=4u5nt^pv#$th3e!!76p5(v@*(u6uzrigxi1e63+ux^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swagprogramme.pythonanywhere.com']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
