6fba13e0b00462ab0e0f3b48dcaff0bd4a1b437f
sheeshmann <schliessmann.lukas@t-online.de>
2025-08-12 15:32:43 +0200

    Router name fixen

--- vorrat/views.py
+++ vorrat/views.py
@@ -17,11 +17,10 @@ class FoodItemViewSet(viewsets.ModelViewSet):
     serializer_class = FoodItemSerializer
     permission_classes = [IsAuthenticated]  # API nur für eingeloggte Nutzer
 
+    queryset = FoodItem.objects.none()  # <- neu
+
     def get_queryset(self):
         return FoodItem.objects.filter(user=self.request.user).order_by('expiration_date')
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
 @login_required
 def overview(request):
