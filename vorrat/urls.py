b56513af469634408ec563410dba469021e8095d
sheeshmann <schliessmann.lukas@t-online.de>
2025-08-12 18:13:52 +0200

    fix

--- vorrat/urls.py
+++ vorrat/urls.py
@@ -4,7 +4,7 @@ from .views import overview, food_add, signup, food_delete, food_change_qty
 urlpatterns = [
     path('', overview, name='overview'),
     path('add/', food_add, name='food-add'),
-    path('item/<int:pk>/delete/', views.food_delete, name='food-delete'),
-    path('item/<int:pk>/change/<int:delta>/', views.food_change_qty, name='food-change-qty'), 
+    path('item/<int:pk>/delete/', food_delete, name='food-delete'),
+    path('item/<int:pk>/change/<int:delta>/', food_change_qty, name='food-change-qty'),
     path('accounts/signup/', signup, name='signup'),
 ]
