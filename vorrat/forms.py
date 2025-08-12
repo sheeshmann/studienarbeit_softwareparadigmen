43addbe1e765a686698433c48c0bd1ddb1984a82
sheeshmann <schliessmann.lukas@t-online.de>
2025-08-12 15:18:00 +0200

    Datumseingabe angepasst

--- vorrat/forms.py
+++ vorrat/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import FoodItem
 
 class FoodItemForm(forms.ModelForm):
+    expiration_date = forms.DateField(
+        input_formats=['%d.%m.%Y', '%Y-%m-%d'],
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
     class Meta:
         model = FoodItem
         fields = ['name', 'quantity', 'unit', 'expiration_date']
