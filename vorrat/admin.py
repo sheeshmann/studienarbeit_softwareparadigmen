c59032bbb2d1861d51b676e28d24c789e3b68025
sheeshmann <schliessmann.lukas@t-online.de>
2025-08-15 17:46:09 +0200

    Modells für Adminseite registrieren

--- vorrat/admin.py
+++ vorrat/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import FoodItem, ItemStat  # ggf. weitere Modelle importieren
 
-# Register your models here.
+admin.site.register(FoodItem)
+admin.site.register(ItemStat)
